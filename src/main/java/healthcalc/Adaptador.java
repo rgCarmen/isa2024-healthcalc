99530c871b94ba9319a0a9b2b7036f4bcb64cb4c
carmen <carmenrodriguezgonzalez0822@gmail.com>
2024-05-02 23:17:43 +0200

    implementación adpatador healthhospital a HealthCalcImp

---
+++ src/main/java/healthcalc/Adaptador.java
@@ -0,0 +1,24 @@
+package healthcalc;
+
+public class Adaptador implements HealthHospital{
+
+    private HealthCalcImpl calc;
+    public Adaptador(HealthCalcImpl h){
+        calc=h;
+    }
+
+    @Override
+    public double bmr(char genero, int edad, float altura, int peso) throws Exception {
+        //peso esta en gramos y debemos pasarlo a kiligramos
+        //altura esta en metros y debemos pasarla a centimetros
+        return (double) calc.basalMetabolicRate(peso/1000f, (int) (altura*100), genero, edad);
+    }
+
+    @Override
+    public int pesoIdeal(char genero, float altura) throws Exception{
+        float result =  calc.idealWeight((int) (altura*100), genero);
+        //el resultado esta en kilogramos y lo tenemos que pasar a gramos
+        return  (int) result*1000;
+    }
+    
+}
--- src/main/java/healthcalc/HealthHospital.java
+++ src/main/java/healthcalc/HealthHospital.java
@@ -2,7 +2,7 @@ package healthcalc;
 
 public interface HealthHospital {
 
-    public double bmr(char genero, int edad, float altura, int peso);
-    public int pesoIdeal(char genero, float altura);
+    public double bmr(char genero, int edad, float altura, int peso) throws Exception;
+    public int pesoIdeal(char genero, float altura) throws Exception;
     
 }
