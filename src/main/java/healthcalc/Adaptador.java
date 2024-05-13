da6c2399af05dbd5a2877f1a7a496e032c8ef196
carmen <carmenrodriguezgonzalez0822@gmail.com>
2024-05-13 09:24:31 +0200

    modificaciones necesarias en el adapatador

--- src/main/java/healthcalc/Adaptador.java
+++ src/main/java/healthcalc/Adaptador.java
@@ -8,17 +8,30 @@ public class Adaptador implements HealthHospital{
     }
 
     @Override
-    public double bmr(Gender genero, int edad, float altura, int peso) throws Exception {
+    public double bmr(char genero, int edad, float altura, int peso) throws Exception {
         //peso esta en gramos y debemos pasarlo a kiligramos
         //altura esta en metros y debemos pasarla a centimetros
-        return (double) calc.basalMetabolicRate(new PersonImpl(peso/1000f, (altura*100), edad, genero));
+        
+        return (double) calc.basalMetabolicRate(new PersonImpl(peso/1000f, (altura*100), edad, getGender(genero)));
     }
 
     @Override
-    public int pesoIdeal(Gender genero, float altura) throws Exception{
-        float result =  calc.idealWeight(new PersonImpl((altura*100), genero));
+    public int pesoIdeal(char genero, float altura) throws Exception{
+        float result =  calc.idealWeight(new PersonImpl((altura*100), getGender(genero)));
         //el resultado esta en kilogramos y lo tenemos que pasar a gramos
         return  (int) result*1000;
+    }
+
+    private Gender getGender(char genero){
+
+        switch (genero) {
+            case 'm':
+                return Gender.MALE;
+            case 'w':
+                return Gender.FEMALE;
+            default:
+                throw new IllegalArgumentException("Género no válido: " + genero);
+        }
     }
     
 }
